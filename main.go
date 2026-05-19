aa4474d44d8400c0adfb31ed23610353eb68ab28
Raffinnaisse <rabbit23113@gmail.com>
2026-05-19 12:01:31 +0300

    write-file-fn

--- main.go
+++ main.go
@@ -25,6 +25,16 @@ func readFile(filename string) ([]User, error) {
 	return response, nil
 }
 
+func writeFile(filename string, content any) error {
+	bytes, err := json.Marshal(content)
+	if err != nil {
+		msg := err.Error()
+		return fmt.Errorf("JSON marshal error: %s", msg)
+	}
+	os.WriteFile(filename, bytes, 0644)
+	return nil
+}
+
 type User struct {
 	Username string
 	Password string
