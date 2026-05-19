8547b14a66790ddd2b659a24df2d76f503fb84ed
Raffinnaisse <rabbit23113@gmail.com>
2026-05-19 12:23:24 +0300

    create-user-fn

---
+++ go.mod
@@ -0,0 +1,5 @@
+module main
+
+go 1.26.2
+
+require golang.org/x/crypto v0.51.0 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 
@@ -14,13 +20,11 @@ func readFile(filename string) ([]User, error) {
 	var response []User
 	content, err := os.ReadFile(filename)
 	if err != nil {
-		msg := err.Error()
-		return []User{}, fmt.Errorf("File reading error: %s", msg)
+		return nil, errorHandler(err)
 	}
 	err = json.Unmarshal(content, &response)
 	if err != nil {
-		msg := err.Error()
-		return []User{}, fmt.Errorf("JSON unmarshal error: %s", msg)
+		return nil, errorHandler(err)
 	}
 	return response, nil
 }
@@ -28,11 +32,47 @@ func readFile(filename string) ([]User, error) {
 func writeFile(filename string, content any) error {
 	bytes, err := json.Marshal(content)
 	if err != nil {
-		msg := err.Error()
-		return fmt.Errorf("JSON marshal error: %s", msg)
+		errorHandler(err)
 	}
 	os.WriteFile(filename, bytes, 0644)
 	return nil
+}
+
+func createUser() error {
+	var users []User
+	fmt.Println("__ Username __")
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		errorHandler(err)
+	}
+	username = strings.TrimSpace(username)
+	fmt.Println("__ Password __")
+	password, err := reader.ReadString('\n')
+	if err != nil {
+		errorHandler(err)
+	}
+	password = strings.TrimSpace(password)
+	response, err := readFile("users.json")
+	if err != nil {
+		errorHandler(err)
+	}
+	err = writeFile("users.json", response)
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		errorHandler(err)
+	}
+	user := User{Username: username, Password: string(hash)}
+	users = append(response, user)
+	err = writeFile("users.json", users)
+	if err != nil {
+		errorHandler(err)
+	}
+	return nil
+}
+
+func errorHandler(err error) error {
+	msg := err.Error()
+	return fmt.Errorf("Error: %s", msg)
 }
 
 type User struct {
